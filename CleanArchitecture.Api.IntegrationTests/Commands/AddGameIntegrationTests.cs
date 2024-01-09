8b5f01fa14bf9d37f5f9ffb06c379c8488d1ca00
jaime-delgado <jdaniel1987@hotmail.com>
2024-01-09 22:36:31 +0100

    Improve integration test

--- CleanArchitecture.Api.IntegrationTests/Commands/AddGameIntegrationTests.cs
+++ CleanArchitecture.Api.IntegrationTests/Commands/AddGameIntegrationTests.cs
@@ -1,10 +1,12 @@
 using AutoFixture;
 using AutoFixture.Xunit2;
+using AutoMapper;
 using CleanArchitecture.Infrastructure;
 using CleanArchitecture.Services.Commands.AddGame;
 using FluentAssertions;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Moq;
@@ -15,37 +17,44 @@ namespace CleanArchitecture.API.IntegrationTests.Commands;
 
 public class AddGameIntegrationTests
 {
-    public sealed class AddGameTests
+    [Theory, AutoData]
+    public async Task Should_add_game(
+        IFixture fixture,
+        AddGameCommand addGameCommand)
     {
-        [Theory, AutoData]
-        public async Task Should_send_command(
-            IFixture fixture,
-            AddGameCommand addGameCommand,
-            Mock<IPublisher> mediatorPublisher)
+        // Arrange
+        await using var application = new CustomWebApplicationFactory();
+        using var client = application.CreateClient();
+
+        var dbContextFactory = application.Services.GetService<IDbContextFactory<DatabaseContext>>() !;
+        using var dbContext = dbContextFactory.CreateDbContext();
+
+        var existingGamesConsole = fixture.Build<Infrastructure.Models.GamesConsole>()
+            .With(c => c.Id, addGameCommand.GamesConsoleId)
+            .Without(c => c.Games)
+            .Create();
+        await dbContext.GamesConsoles.AddAsync(existingGamesConsole);
+        await dbContext.SaveChangesAsync();
+
+        var mapper = application.Services.GetService<IMapper>() !;
+
+        // Act
+        var response = await client.PostAsJsonAsync("api/AddGame", addGameCommand);
+
+        // Assert
+        var actual = await dbContext.Games.SingleAsync(g => g.GamesConsoleId == existingGamesConsole.Id);
+        var expected = new Infrastructure.Models.Game()
         {
-            // Arrange
-            await using var application = new CustomWebApplicationFactory(services =>
-            {
-                services
-                    .Replace(ServiceDescriptor.Transient(_ => mediatorPublisher.Object));
-            });
-            using var client = application.CreateClient();
+            Publisher = addGameCommand.Publisher,
+            GamesConsoleId = addGameCommand.GamesConsoleId,
+            Name = addGameCommand.Name,
+            Price = addGameCommand.Price,
+        };
 
-            var dbContextFactory = application.Services.GetService<IDbContextFactory<DatabaseContext>>() !;
-            using var dbContext = dbContextFactory.CreateDbContext();
-
-            var existingGamesConsole = fixture.Build<Infrastructure.Models.GamesConsole>()
-                .With(c => c.Id, addGameCommand.GamesConsoleId)
-                .Without(c => c.Games)
-                .Create();
-            await dbContext.GamesConsoles.AddAsync(existingGamesConsole);
-            await dbContext.SaveChangesAsync();
-
-            // Act
-            var response = await client.PostAsJsonAsync("api/AddGame", addGameCommand);
-
-            // Assert
-            response.StatusCode.Should().Be(HttpStatusCode.Created);
-        }
+        response.StatusCode.Should().Be(HttpStatusCode.Created);
+        actual.Should().BeEquivalentTo(expected, 
+            opts => opts
+            .Excluding(g => g.Id)
+            .Excluding(g => g.GamesConsole));
     }
 }

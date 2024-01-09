2768ca9adbbf1bf260fbadb1d5a8cff0f142c3cb
jaime-delgado <jdaniel1987@hotmail.com>
2024-01-09 23:03:22 +0100

    Create BaseIntegrationTests and used into AddGameIntegrationTests

---
+++ CleanArchitecture.Api.IntegrationTests/BaseIntegrationTests.cs
@@ -0,0 +1,36 @@
+﻿using AutoFixture;
+using AutoMapper;
+using CleanArchitecture.Infrastructure;
+using CleanArchitecture.Infrastructure.EmailSender;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace CleanArchitecture.API.IntegrationTests;
+
+public class BaseIntegrationTests
+{
+    protected HttpClient _httpClient;
+    protected DatabaseContext _databaseContext;
+    protected IMapper _mapper;
+    protected Mock<ILogger<FakeEmailSender>> _mockEmailLogger;
+
+    public BaseIntegrationTests()
+    {
+        var fixture = new Fixture();
+        _mockEmailLogger = fixture.Create<Mock<ILogger<FakeEmailSender>>>();
+        var application = new CustomWebApplicationFactory(services =>
+        {
+            services
+                    .Replace(ServiceDescriptor.Transient(_ => _mockEmailLogger.Object));
+        });
+        _httpClient = application.CreateClient();
+
+        var dbContextFactory = application.Services.GetService<IDbContextFactory<DatabaseContext>>()!;
+        _databaseContext = dbContextFactory.CreateDbContext();
+
+        _mapper = application.Services.GetService<IMapper>()!;
+    }
+}
--- CleanArchitecture.Api.IntegrationTests/Commands/AddGameIntegrationTests.cs
+++ CleanArchitecture.Api.IntegrationTests/Commands/AddGameIntegrationTests.cs
@@ -1,55 +1,36 @@
 using AutoFixture;
 using AutoFixture.Xunit2;
-using AutoMapper;
-using CleanArchitecture.Infrastructure;
 using CleanArchitecture.Infrastructure.EmailSender;
 using CleanArchitecture.Services.Commands.AddGame;
 using FluentAssertions;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Logging;
 using Moq;
-using System;
 using System.Net;
 using System.Net.Http.Json;
 
 namespace CleanArchitecture.API.IntegrationTests.Commands;
 
-public class AddGameIntegrationTests
+public class AddGameIntegrationTests : BaseIntegrationTests
 {
     [Theory, AutoData]
     public async Task Should_add_game(
         IFixture fixture,
-        AddGameCommand addGameCommand,
-        Mock<ILogger<FakeEmailSender>> mockLogger)
+        AddGameCommand addGameCommand)
     {
         // Arrange
-        await using var application = new CustomWebApplicationFactory(services =>
-        {
-            services
-                    .Replace(ServiceDescriptor.Transient(_ => mockLogger.Object));
-        });
-        using var client = application.CreateClient();
-
-        var dbContextFactory = application.Services.GetService<IDbContextFactory<DatabaseContext>>() !;
-        using var dbContext = dbContextFactory.CreateDbContext();
-
         var existingGamesConsole = fixture.Build<Infrastructure.Models.GamesConsole>()
             .With(c => c.Id, addGameCommand.GamesConsoleId)
             .Without(c => c.Games)
             .Create();
-        await dbContext.GamesConsoles.AddAsync(existingGamesConsole);
-        await dbContext.SaveChangesAsync();
-
-        var mapper = application.Services.GetService<IMapper>() !;
+        await _databaseContext.GamesConsoles.AddAsync(existingGamesConsole);
+        await _databaseContext.SaveChangesAsync();
 
         // Act
-        var response = await client.PostAsJsonAsync("api/AddGame", addGameCommand);
+        var response = await _httpClient.PostAsJsonAsync("api/AddGame", addGameCommand);
 
         // Assert
-        var actual = await dbContext.Games.SingleAsync(g => g.GamesConsoleId == existingGamesConsole.Id);
+        var actual = await _databaseContext.Games.SingleAsync(g => g.GamesConsoleId == existingGamesConsole.Id);
         var expected = new Infrastructure.Models.Game()
         {
             Publisher = addGameCommand.Publisher,
@@ -63,6 +44,6 @@ public class AddGameIntegrationTests
             opts => opts
             .Excluding(g => g.Id)
             .Excluding(g => g.GamesConsole));
-        mockLogger.VerifyLog(mock => mock.LogInformation("Sent mail to {Email} ", "random@email.com"), Times.Once);
+        _mockEmailLogger.VerifyLog(mock => mock.LogInformation("Sent mail to {Email} ", "random@email.com"), Times.Once);
     }
 }
